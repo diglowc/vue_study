212d96aa48d5f3247c7c090135dc6d70db377467
diglowc <81141945+diglowc@users.noreply.github.com>
2024-02-07 04:03:56 +0900

    feat: Pretendard 폰트 추가

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
@@ -15,16 +15,52 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
+@font-face {
+  font-family: "Pretendard";
+  src: url("assets/fonts/Pretendard-Regular.otf");
+  font-weight: 400;
+  font-style: normal;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+@font-face {
+  font-family: "Pretendard-ExtraLight";
+  src: url("assets/fonts/Pretendard-ExtraLight.otf");
+  font-weight: 400;
+  font-style: normal;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+@font-face {
+  font-family: "Pretendard-Light";
+  src: url("assets/fonts/Pretendard-Light.otf");
+  font-weight: 400;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Pretendard-Medium";
+  src: url("assets/fonts/Pretendard-Medium.otf");
+  font-weight: 400;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Pretendard-SemiBold";
+  src: url("assets/fonts/Pretendard-SemiBold.otf");
+  font-weight: 400;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Pretendard-Bold";
+  src: url("assets/fonts/Pretendard-Bold.otf");
+  font-weight: 400;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: "Pretendard-ExtraBold";
+  src: url("assets/fonts/Pretendard-ExtraBold.otf");
+  font-weight: 400;
+  font-style: normal;
 }
 </style>
