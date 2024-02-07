559c8f1a962a3b50356a6d5b6208ca9941ab7bcd
diglowc <81141945+diglowc@users.noreply.github.com>
2024-02-07 17:25:52 +0900

    feat: 포스팅 목록 헤더 컴포넌트 생성

---
+++ src/components/resources/PostingListHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="postingList">
+    <p class="headerTitle">{{ title }}</p>
+    <img class="viewMore" src="@/assets/images/view_more.png" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+interface Props {
+  title: string;
+}
+
+const props = defineProps<Props>();
+
+console.log(props.title);
+</script>
+
+<style lang="scss">
+.postingList {
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.headerTitle {
+  width: fit-content;
+  font-size: 18px;
+  color: #000000;
+  font-family: "Pretendard-SemiBold";
+}
+.viewMore {
+  width: 24px;
+  height: 24px;
+}
+</style>
