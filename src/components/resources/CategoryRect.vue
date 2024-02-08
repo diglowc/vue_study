74aba02c998537e2d1dade849a519a179d46396c
diglowc <81141945+diglowc@users.noreply.github.com>
2024-02-08 23:26:31 +0900

    feat: 카테고리 표시 컴포넌트 생성

---
+++ src/components/resources/CategoryRect.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="categoryRectBox" :style="{ backgroundColor: backColor }">
+    <p class="categoryTitle">{{ title }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+interface Props {
+  title: string;
+  backColor: string;
+}
+
+const props = defineProps<Props>();
+
+console.log(props.title);
+console.log(props.backColor);
+</script>
+
+<style lang="scss">
+.categoryRectBox {
+  width: fit-content;
+  padding: 3px 8px;
+  border-radius: 4px;
+  justify-content: center;
+  align-items: center;
+}
+.categoryTitle {
+  font-size: 14px;
+  color: #ffffff;
+  font-family: "Pretendard";
+  margin-block: 0;
+}
+</style>
