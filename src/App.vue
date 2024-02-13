4d12d149085aa87cba613fb5d420d18942972962
diglowc <81141945+diglowc@users.noreply.github.com>
2024-02-13 23:50:08 +0900

    feat: 로고, 마이페이지, 알림 포함한 메인 헤더 생성

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div class="mainHeader">
+    <nav>
+      <router-link class="logo" to="/home">
+        <p class="logoText">BeVelop</p>
+        <img class="logoMenu" src="@/assets/images/chevron.png" />
+      </router-link>
+    </nav>
+    <nav>
+      <router-link to="/mypage">
+        <img class="mypageIcon" src="@/assets/images/mypage.png" />
+      </router-link>
+      <router-link to="/about">
+        <img class="alarmIcon" src="@/assets/images/alarm.png" />
+      </router-link>
+    </nav>
+  </div>
+
   <router-view />
 </template>
 
@@ -62,5 +75,34 @@
   src: url("assets/fonts/Pretendard-ExtraBold.otf");
   font-weight: 400;
   font-style: normal;
+}
+.mainHeader,
+.logo {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.mainHeader {
+  justify-content: space-between;
+  margin: 11px 20px;
+}
+.logoText {
+  font-size: 22px;
+  font-family: "Pretendard";
+  font-weight: 500;
+  color: #000000;
+}
+.logoMenu {
+  margin-left: 7px;
+  width: 24px;
+  height: 24px;
+}
+.mypageIcon,
+.alarmIcon {
+  width: 24px;
+  height: 24px;
+}
+.alarmIcon {
+  margin-left: 12px;
 }
 </style>
