f2d3e76d562054fb700cf4ec2d040b6f07a0b088
diglowc <81141945+diglowc@users.noreply.github.com>
2024-02-16 21:58:37 +0900

    feat: 기술스택 텍스트박스 컴포넌트 생성

---
+++ src/components/resources/AbilityCategory.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="abilityCategoryBox" :style="{ backgroundColor: backColor }">
+    <p class="abilityCategoryTitle">{{ title }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from "vue";
+
+interface Props {
+  title: string;
+  backColor: string;
+}
+
+const props = defineProps<Props>();
+
+console.log(props.title);
+console.log(props.backColor);
+</script>
+
+<style lang="scss">
+.abilityCategoryBox {
+  width: fit-content;
+  padding: 10px 27px;
+  border-radius: 6px;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 12px;
+  margin-right: 7px;
+}
+.abilityCategoryTitle {
+  font-size: 14px;
+  color: #ffffff;
+  font-family: "Pretendard";
+  margin-block: 0;
+}
+</style>
