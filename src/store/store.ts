b40a8b02baa3ac7fd571d7daa11d0fbc619bf954
diglowc <81141945+diglowc@users.noreply.github.com>
2024-02-23 23:49:42 +0900

    add: 프로필 수정 기능을 위한 전역변수 생성

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,11 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    nickname: "Hyedesignr",
+    introduction: "다양한 서비스 제공을 위해 힘쓰는 장혜리 디자이너입니다.",
+    job: "UX디자이너",
+    field: "서비스디자인",
+    abilities: ["Figma", "Adobe XD", "Zeplin", "Photoshop"],
+  },
 });
